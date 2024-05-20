92e6e7d11a60b34c298d95f07b1447f0670131a9
Yael Hassid <yaelhassid@h-MacBook-Pro-sl-Yael.local>
2024-05-20 13:12:52 +0300

    six

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ session = boto3.Session(
     region_name=os.environ['REGION']
 )
 
-s3 = session.client('s3')
-# s3 = boto3.client('s3')
+# s3 = session.client('s3')
+s3 = boto3.client('s3')
 # s3 = boto3.client('s3', aws_access_key_id= os.environ['aws_access_key_id'], aws_secret_access_key=os.environ['aws_secret_access_key'], region=os.environ['region'])
 obj = s3.get_object(Bucket="best-practice-bucket-of-yaya", Key="hello.gif")
 gifi = obj['Body'].read()
